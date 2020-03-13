db350f4200671ead2621d588a5ef9234f07fbbb2
Jo Redfern <mrsjoredfern@yahoo.co.uk>
2020-03-13 15:34:39 +0000

    only shows top 10 records so buttons dont disappear off page

--- lib/peeps.rb
+++ lib/peeps.rb
@@ -14,7 +14,7 @@ class Peep
   else
     connection = PG.connect(dbname: 'chitter')
   end
-  peeps = connection.exec("SELECT * FROM peeps ORDER BY created_at DESC;")
+  peeps = connection.exec("SELECT * FROM peeps ORDER BY created_at DESC LIMIT 10;")
   peeps.map do |peep|
   Peep.new(id: peep['id'], message: peep['message'], created_at: peep['created_at'])
   end
