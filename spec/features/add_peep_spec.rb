8634e725f1f0fa88982d35b54afcca371f7ba348
Nora van den Heuvel <noravdheuvel@gmail.com>
2020-03-13 11:14:03 +0000

    failing post tweet feature test

--- spec/features/add_peep_spec.rb
+++ spec/features/add_peep_spec.rb
@@ -1,1 +1,10 @@
-require 'pg'
+
+feature 'Posting a peep' do
+  scenario 'Users can post a peep' do
+    visit('/')
+    fill_in('message', with: 'This is my first peep!')
+    click_button('Post')
+
+    expect(page).to have_content 'This is my first peep!'
+  end
+end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -3,7 +3,7 @@ require 'peep'
 describe Peep do
   it "can view all messages" do
     messages = Peep.all
-    expect(messages.length).to eq 1
-    expect(messages).to eq ["This is a peep!"]
+    expect(messages.length).to eq 2
+    expect(messages).to eq ["This is a peep!", "This is a second peep!"]
   end
 end
