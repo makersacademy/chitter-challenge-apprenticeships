af5a60d4832908c2abab022502d1a4029520edef
Nora van den Heuvel <noravdheuvel@gmail.com>
2020-03-13 16:03:57 +0000

    changd timestamp

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -7,6 +7,6 @@ end
 
 def add_rows_to_test_database
   connection = PG.connect(dbname: 'chitter_test')
-  connection.exec("INSERT INTO peeps (message, timestamp) VALUES ('This is a peep!', CURRENT_TIMESTAMP)")
-  connection.exec("INSERT INTO peeps (message, timestamp) VALUES ('This is a second peep!', CURRENT_TIMESTAMP)")
+  connection.exec("INSERT INTO peeps (message, timestamp) VALUES ('This is a peep!', '2020-03-13 11:57:31.08057+00')")
+  connection.exec("INSERT INTO peeps (message, timestamp) VALUES ('This is a second peep!', '2020-03-13 11:57:34.08057+00')")
 end
