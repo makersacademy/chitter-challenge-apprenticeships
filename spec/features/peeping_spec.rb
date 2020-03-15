7dfd31bb04a0b69bed22f4bb03f40e0f9e02f096
Student <student@makerss-air-2.home>
2020-03-15 14:50:43 +0000

    pass_

--- spec/features/peeping_spec.rb
+++ spec/features/peeping_spec.rb
@@ -8,8 +8,8 @@ describe Tweet do
   
 	it 'creates peeps' do 
 		tweet = Tweet.new('Im peeping', '14.11.1988')
-		expect(peep.message).to eq 'Im peeping'
-		expect(peep.date).to eq '14.11.1988'
+		expect(tweet.message).to eq 'Im peeping'
+		expect(tweet.date).to eq '14.11.1988'
   end
   
 end 
