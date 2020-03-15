ec68530e97321bf3f65867617bad93d28e25bbd1
Hodan <hodanahmed0000@gmail.com>
2020-03-15 16:16:46 +0000

    Refactored class spec test

--- spec/peeps_spec.rb
+++ spec/peeps_spec.rb
@@ -4,9 +4,9 @@ require 'pg'
 describe 'Peeps' do
   let(:test) { Peeps.new }
   it 'can view all peeps' do
-    connection = PG.connect(dbname: 'chitter_test')
-    connection.exec("INSERT INTO peeps (message) VALUES ('This is a peep!');")
+    setup_test_database
+    add_row_to_test_database
     test1 = test.view_all
-    expect(test1).to include("This is a peep!")
+  expect(test1).to include("This is a peep!")
   end
 end
