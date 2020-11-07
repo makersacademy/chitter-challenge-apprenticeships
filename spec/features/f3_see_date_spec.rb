e5566c104ee9e1ac2edc726b8b3eb331413df9e2
GDCStoney <g.stoney@btinternet.com>
2020-11-07 10:18:12 +0000

    corrected date order test

--- spec/features/f3_see_date_spec.rb
+++ spec/features/f3_see_date_spec.rb
@@ -7,4 +7,15 @@ feature 'Able to see date of post' do
 
     within('li') { expect(page).to have_content(/\d{2}-...-\d{4}/) }
   end
+
+  scenario 'see dates in reverse order in list view' do
+    result1 = add_row_to_test_database
+    result2 = add_row_to_test_database
+    result3 = add_row_to_test_database
+    visit('/')
+
+    expect(page.all('li')[0].value).to eq result3[0]['id']
+    expect(page.all('li')[1].value).to eq result2[0]['id']
+    expect(page.all('li')[2].value).to eq result1[0]['id']
+  end
 end
