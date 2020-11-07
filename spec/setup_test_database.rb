ecded3958063f520415adb7949d20ad62bad7c74
GDCStoney <g.stoney@btinternet.com>
2020-11-07 15:44:43 +0000

    just cleaning up rubocop stuff

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -13,14 +13,9 @@ end
 
 def add_test_data_to_database
   connection = PG.connect(dbname: 'chitter_test')
-  connection.exec("INSERT INTO cheeps(message) VALUES ('This is a test cheep!')
-    RETURNING id, message, date_post;")
-  connection.exec("INSERT INTO cheeps(message) VALUES ('More information being added!')
-    RETURNING id, message, date_post;")
-  connection.exec("INSERT INTO cheeps(message) VALUES ('We could use this for FILTERing!')
-    RETURNING id, message, date_post;")
-  connection.exec("INSERT INTO cheeps(message) VALUES ('Or this for FILTER testing!')
-    RETURNING id, message, date_post;")
-  connection.exec("INSERT INTO cheeps(message) VALUES ('Not to get carried away with that!')
-    RETURNING id, message, date_post;")
+  connection.exec("INSERT INTO cheeps(message) VALUES ('This is a test cheep!');")
+  connection.exec("INSERT INTO cheeps(message) VALUES ('More information being added!');")
+  connection.exec("INSERT INTO cheeps(message) VALUES ('We could use this for FILTERing!');")
+  connection.exec("INSERT INTO cheeps(message) VALUES ('Or this for FILTER testing!');")
+  connection.exec("INSERT INTO cheeps(message) VALUES ('Not to get carried away with that!');")
 end
