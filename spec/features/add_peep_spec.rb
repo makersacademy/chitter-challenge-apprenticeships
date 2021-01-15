1c8811a36de6b451917e96edc6317b0d6a5fc7ef
Bianca <biannca.iordache@gmail.com>
2021-01-15 12:00:05 +0000

    pass feature test and create a new post route

--- spec/features/add_peep_spec.rb
+++ spec/features/add_peep_spec.rb
@@ -1,7 +1,7 @@
 feature 'Add a new peep' do
   scenario 'A user can post a message to Chitter' do
     visit('peeps/new')
-    fill_in('peep', with: 'This is my first message!')
+    fill_in('message', with: 'This is my first message!')
     click_button 'Submit'
     expect(page).to have_content 'This is my first message!'
   end
