487cd5fa7c2f17241281d972aa11b8845ff9eaa7
girl-interrupted <d.pavliuchyk@gmail.com>
2021-02-19 15:36:02 +0000

    rubocop indentation fixes

--- lib/peep.rb
+++ lib/peep.rb
@@ -2,16 +2,16 @@ require 'pg'
 
 class Peep
 
-	 def self.view_all
- 		 if ENV['ENVIRONMENT'] == 'test'
+	def self.view_all
+   	if ENV['ENVIRONMENT'] == 'test'
   			 connection = PG.connect(dbname: 'chitter_test')
   		else
   			 connection = PG.connect(dbname: 'chitter')
   		end
-   
+     
     result = connection.exec('SELECT * FROM peeps')
     result.map { |peep| peep['message'] }
-   end
+  end
 
   def self.create(message:)
     if ENV['ENVIRONMENT'] == 'test'
