f7e3ce45b995fc427168b82903988e834c1b383c
Vincenzo Florio <vincenzoflorio31@gmail.com>
2021-02-19 11:17:18 +0000

    Add Peeps  Model and index view to app.rb

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'sinatra/base'
+require './lib/chitter'
 
 class Chitter < Sinatra::Base
   get '/test' do
     'Test page'
+  end
+
+  get '/peeps' do
+    @peeps = Peeps.all
+    erb :'peeps/index'
   end
 
   run! if app_file == $0
