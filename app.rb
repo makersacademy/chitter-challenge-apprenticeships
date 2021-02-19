422b212b2cca52920c0eb22ca5a59638bd5d2416
AlistairTait12 <alistair.tait1@gmail.com>
2021-02-19 12:10:32 +0000

    GREEN can view test peep in index as a value from an INSTANCE of Peep class

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require_relative './lib/peep.rb'
 
 class Chitter < Sinatra::Base
   get '/test' do
@@ -6,6 +7,7 @@ class Chitter < Sinatra::Base
   end
 
   get '/' do
+    @test_peep = Peep.new(user: 'Testguy', content: 'This is a peep!', timestamp: '19/02/2021 1148')
     erb(:index)
   end
 
