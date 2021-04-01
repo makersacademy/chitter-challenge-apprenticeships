7977f859bca32cd93e765718d6a13096732046bf
Alice Lui <aytlui@gmail.com>
2021-04-01 12:01:16 +0100

    test environment setup

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -1,3 +1,5 @@
+require 'pg'
+
 feature 'viewing index page' do
   scenario 'User can see the index page' do
     visit('/')
@@ -7,6 +9,9 @@ end
 
 feature 'viewing peeps' do
   scenario 'User can see the peeps' do
+    connection = PG.connect(dbname: 'chitter_test')
+    connection.exec("INSERT INTO peeps VALUES (1, 'This is a peep!');")
+    
     visit('/peeps')
 
     expect(page).to have_content "This is a peep!"
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -3,7 +3,12 @@ require 'peep'
 describe Peep do
   describe '.all' do
     it 'returns a list of all the peeps' do
+      connection = PG.connect(dbname: 'chitter_test')
+
+      connection.exec("INSERT INTO peeps (message) VALUES ('This is a peep!');")
+
       peeps = Peep.all
+
       expect(peeps).to include("This is a peep!")
     end
   end
