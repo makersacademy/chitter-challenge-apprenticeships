269f0e148a357cd76baa31bf322d6414b392809a
rahat <rahat.ahmed1@outlook.com>
2021-04-05 08:38:57 +0100

    Testing with the test Database

--- app.rb
+++ app.rb
@@ -1,10 +1,8 @@
 require 'sinatra/base'
 require './lib/peep'
 
+
 class Chitter < Sinatra::Base
-  configure :development do
-    register Sinatra::Reloader
-  end
   
   get '/' do
   end
@@ -12,6 +10,13 @@ class Chitter < Sinatra::Base
   get '/peeps' do
     @peeps = Peep.all
     erb :index
+  end
+
+  post '/peeps' do
+    message = params['message']
+    connection = PG.connect(dbname: 'chitter_test')
+    connection.exec("INSERT INTO peeps (message) VALUES('#{message}')")
+    redirect '/peeps'
   end
 
   run! if app_file == $0
--- spec/features/viewing_messages_spec.rb
+++ spec/features/viewing_messages_spec.rb
@@ -8,7 +8,20 @@ feature 'Viewing Messages' do
     connection = PG.connect(dbname: 'chitter_test')
     connection.exec("INSERT INTO peeps (message) values ('This is a peep!');")
     
-    visit ('/peeps')
+    visit('/peeps')
+    expect(page).to have_content "This is a peep!"
+  end
+end
+
+#As a Maker
+#So that I can let people know what I am doing  
+#I want to post a message (peep) to chitter
+
+feature 'Posting messages' do
+  scenario 'User can post a message' do
+    visit('/peeps')
+    fill_in :message, with: 'This is a peep!'
+    click_button 'Post'
     expect(page).to have_content "This is a peep!"
   end
 end
