8fe12710b109d062a1427d08b97a9b3d2e3306b2
rahat <rahat.ahmed1@outlook.com>
2021-04-05 07:41:48 +0100

    Setting up test env

--- spec/features/viewing_messages_spec.rb
+++ spec/features/viewing_messages_spec.rb
@@ -5,6 +5,9 @@
 
 feature 'Viewing Messages' do
   scenario 'Users can view messages' do
+    connection = PG.connect(dbname: 'chitter_test')
+    connection.exec("INSERT INTO peeps (message) values ('This is a peep!');")
+    
     visit ('/peeps')
     expect(page).to have_content "This is a peep!"
   end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -2,6 +2,9 @@ require 'peep'
 
 describe Peep do
   it 'returns all peeps' do
+    connection = PG.connect(dbname: 'chitter_test')
+    connection.exec("INSERT INTO peeps (message) values ('This is a peep!');")
+    
     peeps = Peep.all
 
     expect(peeps).to include("This is a peep!")
