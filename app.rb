8ed3165e40d0354d26a42c01b5d169ea5d7880c1
Juel07 <juel.braganza97@gmail.com>
2021-06-25 00:56:06 +0100

    create files

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
-require 'sinatra/base'
+require "sinatra/base"
+require "sinatra/reloader"
 
 class Chitter < Sinatra::Base
-  get '/test' do
-    'Test page'
+  get "/test" do
+    "Test page"
   end
 
   run! if app_file == $0
