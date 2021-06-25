11bd983d6857934c98a345ee4e2ab23d66c0788c
Hannah <hannah.v.d.wijngaard@gmail.com>
2021-06-25 11:51:37 +0100

    Pclass method .all test setup

--- spec/features/index_page_spec.rb
+++ spec/features/index_page_spec.rb
@@ -1,7 +1,8 @@
 feature 'Index page' do
   scenario 'index page displays peeps' do
+    add_row_to_test_database
     visit '/'
-    expect(page).to have_content "This is the first peep!"
+    expect(page).to have_content "This is a peep!"
   end
 end
 
--- spec/peeps_spec.rb
+++ spec/peeps_spec.rb
@@ -1,15 +1,15 @@
-# require 'peeps'
+require 'peeps'
 
-# describe Peeps do
+describe Peeps do
 
-#   describe '.all' do
-#     it 'returns all peeps' do
-#       connection = PG.connect(dbname: 'chitter_test')
+  describe '.all' do
+    it 'returns a message from the db' do
+      connection = PG.connect(dbname: 'chitter_test')
+      connection.exec("INSERT INTO peeps (message) values ('This is a peep!');")
+      result = connection.exec('SELECT * FROM peeps;')
+      expect(result[0]['message']).to eq "This is a peep!"
+    end
+  end
 
 
-
-#     end
-#   end
-
-
-# end
+end
