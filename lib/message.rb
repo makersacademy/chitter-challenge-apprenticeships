1e0898a1e78ec7288c663ec0703f7044c87ca820
anitaRead <areadman91@gmail.com>
2021-06-25 15:10:27 +0100

    User Story 3 - Add date to messages

--- lib/message.rb
+++ lib/message.rb
@@ -1,6 +1,17 @@
 require 'pg'
 
 class Message
+
+    attr_reader :id, :message, :date
+
+    CURRENT_DATE = Time.new.year.to_s + "-" + Time.new.month.to_s + "-" + Time.new.day.to_s
+
+    def initialize(id:, message:, date:)
+        @id  = id
+        @message = message
+        @date = date
+    end
+
     def self.all
 
         if ENV['ENVIRONMENT'] == 'test'
@@ -11,7 +22,7 @@ class Message
 
         result = connection.exec('SELECT * FROM peeps')
         result.map do |message| 
-            Message.new(id: message['id'], message: message['message']) 
+            Message.new(id: message['id'], message: message['message'], date: message['date']) 
         end
 
     end
@@ -23,17 +34,12 @@ class Message
         #     connection = PG.connect(dbname: 'chitter')
         end
 
-        result = connection.exec("INSERT INTO peeps (message) VALUES('#{message}') RETURNING id, message;")
-        Message.new(id: result[0]['id'], message: result[0]['message'])
+        result = connection.exec("INSERT INTO peeps (message, date) VALUES('#{message}', '#{CURRENT_DATE}') RETURNING id, message, date;")
+        Message.new(id: result[0]['id'], message: result[0]['message'], date: result[0]['date'])
 
     end
 
-    attr_reader :id, :message
-
-    def initialize(id:, message:)
-        @id  = id
-        @message = message
-    end
+    
 end
 
 
