0ca85ecde0773d81bd3b879bf2808bfafc98ca8e
Mike Connolly <mjc86@hotmail.co.uk>
2021-06-25 11:21:59 +0100

    initial test passing

--- app.rb
+++ app.rb
@@ -1,13 +1,14 @@
 require 'sinatra/base'
-require 'sinatra/reloader' if development?
 
 class Chitter < Sinatra::Base
+  require 'sinatra/reloader' if development?
+
   configure :development do
     register Sinatra::Reloader
   end
 
   enable :sessions
-  
+
   get '/test' do
     'Test page'
   end
