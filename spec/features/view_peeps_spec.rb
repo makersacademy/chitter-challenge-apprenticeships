8f1cbab446bd1d087c236333b951e12e1deca94b
Leslie <leslie.liszcz@gmail.com>
2021-06-25 15:02:19 +0100

    test environment working

--- spec/features/view_peeps_spec.rb
+++ spec/features/view_peeps_spec.rb
@@ -1,5 +1,11 @@
 feature 'View all peeps' do
   scenario 'A user can see all peeps in browser' do
+    connection = PG.connect(dbname: 'chitter_test')
+
+    connection.exec("INSERT INTO peeps VALUES(1, 'This is a peep!');")
+    connection.exec("INSERT INTO peeps VALUES(2, 'Welcome to Chitter!');")
+    connection.exec("INSERT INTO peeps VALUES(3, 'My first peep!');")
+    
     visit('/peeps')
 
     expect(page).to have_content "This is a peep!"
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -3,11 +3,17 @@ require 'peep'
 describe Peep do
   describe '.all' do
     it 'returns all peeps' do
+      connection = PG.connect(dbname: 'chitter_test')
+
+      connection.exec("INSERT INTO peeps (message) VALUES('This is a peep!');")
+      connection.exec("INSERT INTO peeps (message) VALUES('Welcome to Chitter!');")
+      connection.exec("INSERT INTO peeps (message) VALUES('My first peep!');")
+
       peeps = Peep.all
 
-      expect(peeps).to include("This is a peep!")
-      expect(peeps).to include("Welcome to Chitter!")
-      expect(peeps).to include("My first peep!")
+      expect(peeps).to include('This is a peep!')
+      expect(peeps).to include('Welcome to Chitter!')
+      expect(peeps).to include('My first peep!')
     end
 
   end
