2d4be8afb32736a288e1a8a8df44e2eca2a19e59
manu <manuelmoualek@gmail.com>
2021-08-13 14:25:44 +0100

    added feature test for adding a peep + index page form

--- app.rb
+++ app.rb
@@ -1,10 +1,15 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative 'lib/peep.rb'
 
 class Chitter < Sinatra::Base
   
   configure :development do
     register Sinatra::Reloader
+  end
+
+  get '/' do
+    erb :index
   end
 
   get '/test' do
--- spec/features/feature_test_spec.rb
+++ spec/features/feature_test_spec.rb
@@ -14,7 +14,7 @@ feature '/peeps route' do
     connection.exec("INSERT INTO peeps (message) VALUES('We''re going to build a well!');")
     connection.exec("INSERT INTO peeps (message) VALUES('Noone needs to pay for it- we got you!');")
     connection.exec("INSERT INTO peeps (message) VALUES('Hang on who needs wells these days anyways?');")
-    
+
     visit('/peeps')
 
     expect(page).to have_content("This is a peep!")
@@ -23,4 +23,13 @@ feature '/peeps route' do
     expect(page).to have_content("Hang on who needs wells these days anyways?")
   
   end
-end    
+end
+
+feature 'user can add a peep' do
+  scenario 'user can add a peep message' do
+    visit('/')
+    fill_in('message', with: 'Yoyoyo')
+    click_on('Peep')
+    expect(page).to have_content "Yoyoyo"
+  end  
+end
