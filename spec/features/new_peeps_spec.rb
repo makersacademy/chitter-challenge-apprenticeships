0db1c023da62950fc6ec46bd382b2f38ecd3deeb
Wesley Hindle <wesleyhindle@gmail.com>
2021-08-13 14:47:03 +0100

    Wrote passing test for writing a new peep. Test coverage has dropped, need to resolve (86%)

--- spec/features/new_peeps_spec.rb
+++ spec/features/new_peeps_spec.rb
@@ -1,7 +1,11 @@
 feature "new peeps page" do 
   scenario "able to submit a new peep" do 
     visit '/peeps/new'
+    conn = PG.connect(dbname: 'chitter_test')
+    conn.exec("INSERT INTO peeps (message) VALUES ('Test peep for FEATURE test 1.');") 
 
-    expect(page).to have_content("This is the test peep for the new peep page.")
+    visit '/peeps'
+
+    expect(page).to have_content("Test peep for FEATURE test 1.")
   end 
 end 
