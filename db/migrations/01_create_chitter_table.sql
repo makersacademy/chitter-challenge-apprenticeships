486b184999e2d9723eedbd46d02ba98417715e4f
manu <manuelmoualek@gmail.com>
2021-08-13 16:07:44 +0100

    saved the SQL commands to add an automated timestamp

--- db/migrations/01_create_chitter_table.sql
+++ db/migrations/01_create_chitter_table.sql
@@ -3,4 +3,4 @@ CREATE TABLE peeps(id SERIAL PRIMARY KEY, message VARCHAR(60));
 -- Add timestamp
 
 ALTER TABLE peeps ADD COLUMN created_at TIMESTAMP;
-ALTER TABLE mytable ALTER COLUMN created_at SET DEFAULT now();
+ALTER TABLE peeps ALTER COLUMN created_at SET DEFAULT now();
