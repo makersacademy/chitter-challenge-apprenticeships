abe511374cae2c7bbcfecae25ac024d7a1ea8d2f
Ben Davis <ben.david.davis16@gmail.com>
2021-08-13 11:52:14 +0100

    Test database setup and tests amended

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -1,15 +1,16 @@
-require 'pg'
-
-feature 'Viewing messages' do
-  scenario 'A user can see all messages' do
+describe '.all' do
+  it 'returns a list of messages' do
     connection = PG.connect(dbname: 'chitter_test')
+
+    # Add the test data
     
     connection.exec("INSERT INTO peeps (message) VALUES ('Hi everyone');")
     connection.exec("INSERT INTO peeps (message) VALUES ('Hope you are enjoying Chitter as much as me');")
-    
-    visit('/messages')
 
-    expect(page).to have_content "Hi everyone"
-    expect(page).to have_content "Hope you are enjoying Chitter as much as me"
+    messages = Message.all
+
+    expect(messages).to include('Hi everyone')
+    expect(messages).to include('Hope you are enjoying Chitter as much as me')
+
   end
-end 
+end
