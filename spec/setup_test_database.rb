81c196b832eb02fab19b28cf9bcad94b4928cc8b
Roisin <ramaguire22@gmail.com>
2021-08-13 15:58:16 +0100

    database set up

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -1,11 +1,11 @@
 require 'pg'
 
 def setup_test_database
-  connection = PG.connect(dbname: 'chitter_test')
+  connection = PG.connect(dbname: 'chitter')
   connection.exec("TRUNCATE peeps;")
 end
 
 def add_row_to_test_database
-  connection = PG.connect(dbname: 'chitter_test')
+  connection = PG.connect(dbname: 'chitter')
   connection.exec("INSERT INTO peeps (message) values ('This is a peep!');")
 end
