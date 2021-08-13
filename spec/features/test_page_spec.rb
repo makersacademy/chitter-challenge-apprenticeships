8fca6830ae5cfaaccf38a98b382ad1e4253cf4ed
manu <manuelmoualek@gmail.com>
2021-08-13 11:09:15 +0100

    added peeps route feature test

--- spec/features/test_page_spec.rb
+++ spec/features/test_page_spec.rb
@@ -4,3 +4,15 @@ feature 'Viewing test page' do
     expect(page).to have_content "Test page"
   end
 end
+
+feature '/peeps route' do
+  scenario 'displays all peeps' do
+    visit('/peeps')
+
+    expect(page).to have_content("This is a peep!")
+    expect(page).to have_content("We're going to build a well!")
+    expect(page).to have_content("Noone needs to pay for it- we got you!")
+    expect(page).to have_content("Hang on who needs wells these days anyways?")
+  
+  end
+end    
