ac2aab3acc6010df29afe5dde696e26f9f3d7550
manu <manuelmoualek@gmail.com>
2021-08-13 11:37:01 +0100

    passed the all method unit test with regular db

--- lib/peep.rb
+++ lib/peep.rb
@@ -4,6 +4,14 @@ require 'pg'
 class Peep
 
   def self.all
+    connection = PG.connect(dbname:"chitter")
+    result = connection.exec("SELECT * FROM peeps")
+    peeps_list = []
+    
+    result.each do |peep|
+      peeps_list.push(peep["message"])
+    end
+    peeps_list
   end  
-  
+
 end
