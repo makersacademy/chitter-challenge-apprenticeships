d487ccde7bad479af2efacef7067332f3207da20
manu <manuelmoualek@gmail.com>
2021-08-13 11:45:35 +0100

    refactored the all method unit tests to query test database and added logic within all method

--- lib/peep.rb
+++ lib/peep.rb
@@ -4,7 +4,7 @@ require 'pg'
 class Peep
 
   def self.all
-    connection = PG.connect(dbname:"chitter")
+    connection = PG.connect(dbname:"chitter_test")
     result = connection.exec("SELECT * FROM peeps")
     peeps_list = []
     
