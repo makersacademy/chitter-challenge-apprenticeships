1231fe64a82825c750f74dfcd2546a0993da9080
manu <manuelmoualek@gmail.com>
2021-08-13 11:17:15 +0100

    passed the peeps route feature test by hardcoding urls in controller

--- app.rb
+++ app.rb
@@ -1,8 +1,21 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
 
 class Chitter < Sinatra::Base
+  
+  configure :development do
+    register Sinatra::Reloader
+  end
+
   get '/test' do
     'Test page'
+  end
+
+  get '/peeps' do
+    "This is a peep!\n\n
+    We're going to build a well!\n
+    Noone needs to pay for it- we got you!\n
+    Hang on who needs wells these days anyways?"
   end
 
   run! if app_file == $0
