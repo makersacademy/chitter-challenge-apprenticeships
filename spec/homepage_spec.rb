1ccf039f470cdf303486f7c27d62252a01a89770
Dylan <Dylan@eequ.org>
2021-08-14 20:14:01 +0100

    Added a good test that I learnt a lot writing (the create peep test)

--- spec/homepage_spec.rb
+++ spec/homepage_spec.rb
@@ -2,9 +2,16 @@ require 'homepage'
 
 describe "HomePage" do
 
-  it 'returns a list of bookmarks' do
+  it 'returns a list of posts' do
     connection = PG.connect(dbname: 'chitter_test')
     homepage = HomePage.all
     expect(homepage.length).to eq 1 #make this better
+  end
+  it 'creates a post' do
+    connection = PG.connect(dbname: 'chitter_test')
+    connection.exec("TRUNCATE peeps;")
+    connection.exec("INSERT INTO peeps (message, date) VALUES('post', '#{Time.now}');")
+    result = connection.exec("SELECT * FROM peeps;").to_a
+    expect(result[0]['message']).to eq('post')
   end
 end
