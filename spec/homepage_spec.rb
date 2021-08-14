243d57175f3882854252110dc7d124181c26456a
Dylan <Dylan@eequ.org>
2021-08-14 20:27:17 +0100

    addded another test for listing peeps

--- spec/homepage_spec.rb
+++ spec/homepage_spec.rb
@@ -4,8 +4,16 @@ describe "HomePage" do
 
   it 'returns a list of posts' do
     connection = PG.connect(dbname: 'chitter_test')
-    homepage = HomePage.all
-    expect(homepage.length).to eq 1 #make this better
+    connection.exec("TRUNCATE peeps;")
+    connection.exec("INSERT INTO peeps (message, date) VALUES('post', '#{Time.now}');")
+    connection.exec("INSERT INTO peeps (message, date) VALUES('post2', '#{Time.now}');")
+    connection.exec("INSERT INTO peeps (message, date) VALUES('post3', '#{Time.now}');")
+    connection.exec("INSERT INTO peeps (message, date) VALUES('post4', '#{Time.now}');")
+    result = connection.exec("SELECT * FROM peeps;").to_a
+    expect(result[0]['message'].length).to eq(4)
+    expect(result[1]['message'].length).to eq(5)
+    expect(result[2]['message'].length).to eq(5)
+    expect(result[3]['message'].length).to eq(5)
   end
   it 'creates a post' do
     connection = PG.connect(dbname: 'chitter_test')
