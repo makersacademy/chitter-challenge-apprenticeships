7a5f5641d4ca1c2cc9fe477784bc7e014ef7947c
Michael PS <michael.plasomscott@hotmail.co.uk>
2021-08-14 17:15:56 +0100

    refactored to have test env

--- spec/features/view_peeps_spec.rb
+++ spec/features/view_peeps_spec.rb
@@ -1,5 +1,10 @@
+require './spec/setup_test_database'
+
 feature 'Viewing peeps' do
   scenario 'A user can see peeps' do
+    setup_test_database
+    connection = PG.connect(dbname: 'chitter_test')
+    connection.exec("INSERT INTO peeps (message) VALUES ('This is a peep!');")
     visit('/home')
 
     expect(page).to have_content "This is a peep!"
--- spec/view_peeps_spec.rb
+++ spec/view_peeps_spec.rb
@@ -3,8 +3,10 @@ require 'peep'
 describe Peep do
   describe '.all' do
     it 'returns all peeps' do
-      peeps = Peep.all
+      connection = PG.connect(dbname: 'chitter_test')
+      connection.exec("INSERT INTO peeps (message) VALUES('This is a peep!');")
 
+      peeps = Peep.all
       expect(peeps).to include("This is a peep!")
     end
   end
