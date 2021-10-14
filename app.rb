fc35729bc6aaed8018676a39ed14f5117add562d
michelle <michelle.watkins@engineering.digital.dwp.gov.uk>
2021-10-14 10:22:07 +0100

    Wrote failing test for '/' page, solved in simplest way

--- app.rb
+++ app.rb
@@ -1,8 +1,17 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
 
 class Chitter < Sinatra::Base
+  # configure :development do
+  #   register Sinatra::Reloader
+  # end  
+
   get '/test' do
     'Test page'
+  end
+
+  get '/' do
+    'Chitter Messages'
   end
 
   run! if app_file == $0
