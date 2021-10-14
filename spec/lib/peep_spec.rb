638414cc5801a46a5e1277d2dff98fb3703c828a
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-14 17:25:16 +0100

    test(peep): add test for formatting the date correctly

--- spec/lib/peep_spec.rb
+++ spec/lib/peep_spec.rb
@@ -11,4 +11,11 @@ describe Peep do
       expect(peep.message).to eq 'This is a peep'
     end
   end
+
+  describe "#format_date" do
+    it 'formats the date in the correct format' do
+      date = Peep.format_date('2021-10-14')
+      expect(date).to eq '14/10/21'
+    end
+  end
 end
