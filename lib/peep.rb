33aa924134068939effeca3d79e2900eb39237ce
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-14 17:38:08 +0100

    feat(peep): return most recent peeps first, using SQL commands

--- lib/peep.rb
+++ lib/peep.rb
@@ -8,14 +8,13 @@ class Peep
   end
   
   def self.all
-    response = DatabaseConnection.query("SELECT * FROM peeps;")
+    response = DatabaseConnection.query("SELECT * FROM peeps ORDER BY date DESC, id;")
     response.map { | response | Peep.new(id: response['id'], message: response['message'], date: response['date']) }
   end
 
   # Keeping the date in SQL format when creating object
   # Using this method to format on the main page
   # Can maybe make this private/differnt class and instantiate with formatted date?
-  
   def self.format_date(date)
     date = Date.strptime(date, '%Y-%m-%d')
     date.strftime('%d/%m/%y')
