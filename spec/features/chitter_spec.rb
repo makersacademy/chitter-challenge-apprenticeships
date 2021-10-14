1ba8ec9b30cbb4549202577c3567d2987872baf1
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-14 17:37:12 +0100

    test(feature): test that most recent peeps shown first

--- spec/features/chitter_spec.rb
+++ spec/features/chitter_spec.rb
@@ -1,10 +1,20 @@
 require 'pg'
 
 feature 'View all peeps' do
-  scenario 'Database is prepopulated, user is displayed peeps' do
-    DatabaseConnection.query("INSERT INTO peeps(message) VALUES('This is a peep');")
-    
-    visit('/')
-    expect(page).to have_content 'This is a peep'
+  context 'Database is prepopulated' do
+    scenario 'User is displayed a peep' do
+      DatabaseConnection.query("INSERT INTO peeps(message) VALUES('This is a peep');")
+      
+      visit('/')
+      expect(page).to have_content 'This is a peep'
+    end
+
+    scenario 'User is displayed peeps in chronological order' do
+      DatabaseConnection.query("INSERT INTO peeps(message, date) VALUES('This is an old peep', '2021-09-10');")
+      DatabaseConnection.query("INSERT INTO peeps(message, date) VALUES('This is a newer peep', '2021-10-10');")
+
+      visit('/')
+      expect(page).to have_content "This is a newer peep\n10/10/21\nThis is an old peep\n10/09/21"
+    end
   end
 end
