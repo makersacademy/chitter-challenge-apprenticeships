b50bd9fb5b5c37bbce250164a8dc3dd18cffb391
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-14 17:30:12 +0100

    feat(peep): add method to format date nicely

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Peep
   def initialize(id:, message:, date:)
     @id = id
@@ -8,6 +10,15 @@ class Peep
   def self.all
     response = DatabaseConnection.query("SELECT * FROM peeps;")
     response.map { | response | Peep.new(id: response['id'], message: response['message'], date: response['date']) }
+  end
+
+  # Keeping the date in SQL format when creating object
+  # Using this method to format on the main page
+  # Can maybe make this private/differnt class and instantiate with formatted date?
+  
+  def self.format_date(date)
+    date = Date.strptime(date, '%Y-%m-%d')
+    date.strftime('%d/%m/%y')
   end
 
   attr_reader :id, :message, :date
