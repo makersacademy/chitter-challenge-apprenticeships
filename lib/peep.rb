ad5c3a99629fa1c3c200e06100ba381e3a9871be
Sam Thoburn <samuel.j.thoburn@outlook.com>
2021-10-15 10:47:56 +0100

    Fixed linting issues

--- lib/peep.rb
+++ lib/peep.rb
@@ -17,12 +17,14 @@ class Peep
 
   def self.create(message, username, date)
     result = DatabaseConnection.query(
-      'INSERT INTO peeps (message, username, date) VALUES($1, $2, $3) RETURNING id, message, username, date;', [message, username, date])
+      'INSERT INTO peeps (message, username, date) VALUES($1, $2, $3) RETURNING id, message, username, date;', [
+        message, username, date])
     Peep.new(result[0]['id'], result[0]['message'], result[0]['username'], result[0]['date'])
   end
 
   def self.filter(keyword)
-    result = DatabaseConnection.query("SELECT * FROM peeps WHERE message LIKE ('%#{keyword.capitalize}%') OR message LIKE ('%#{keyword.downcase}%') OR message LIKE ('%#{keyword.upcase}%') ORDER BY id DESC")
+    result = DatabaseConnection.query(
+      "SELECT * FROM peeps WHERE message LIKE ('%#{keyword.capitalize}%') OR message LIKE ('%#{keyword.downcase}%') OR message LIKE ('%#{keyword.upcase}%') ORDER BY id DESC")
     result.map { |peep| Peep.new(peep['id'], peep['message'], peep['username'], peep['date']) }
   end
 end
