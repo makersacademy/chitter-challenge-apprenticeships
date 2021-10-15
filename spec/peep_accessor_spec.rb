4b74306b950ecab8dda21a84b95e0be2e6d01f8b
Tom Baxter <thomas99baxter@gmail.com>
2021-10-15 11:29:43 +0100

    rubocop + add DB helper

--- spec/peep_accessor_spec.rb
+++ spec/peep_accessor_spec.rb
@@ -10,4 +10,12 @@ describe PeepAccessor do
       expect(described_class.all.length).to eq(1)
     end
   end
+
+  describe "#add" do
+    it "should add another peep to the db" do
+      expect{described_class.add_peep}.to change{
+        described_class.all.length
+      }.from(1).to(2)
+    end
+  end
 end
