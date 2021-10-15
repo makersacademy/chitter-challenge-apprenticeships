0e9865eb9908d4908c1671196dd2f94459f4f22d
Patrick Chan <q_p18@hotmail.com>
2021-10-15 20:03:04 +0100

    Connect to chitter_test db, insert message, redirect to '/peeps' within POST '/peeps'

--- app.rb
+++ app.rb
@@ -20,5 +20,12 @@ class Chitter < Sinatra::Base
     erb :'peeps/new'
   end
 
+  post '/peeps' do
+    message = params['message']
+    connection = PG.connect(dbname: 'chitter_test')
+    connection.exec("INSERT INTO peeps (message) VALUES('#{message}')")
+    redirect '/peeps'
+  end
+
   run! if app_file == $0
 end
