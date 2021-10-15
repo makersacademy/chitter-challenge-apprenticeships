0aa07e888c42880e30058a1f3d4fba9fe91434a8
Leo Brown <leo_brown_1@hotmail.com>
2021-10-15 11:14:33 +0100

    Changed peeps_spec.rb test to use test database

--- lib/peeps.rb
+++ lib/peeps.rb
@@ -2,8 +2,12 @@ class Peeps
   # def initialize
   # end
 
-  def all
-    connection = PG.connect(dbname: "chitter")
+  def self.all
+    if ENV["ENVIRONMENT"] == "test"
+      connection = PG.connect(dbname: "chitter_test")
+    else
+      connection = PG.connect(dbname: "chitter")
+    end
     result = connection.exec("SELECT message FROM peeps;")
     result.map { |peep| peep["message"] }
   end
--- spec/peeps_spec.rb
+++ spec/peeps_spec.rb
@@ -1,10 +1,12 @@
 require_relative "../lib/peeps"
+require_relative "./setup_test_database"
 
 describe Peeps do
   describe "#.all" do
     it "returns a list of all peeps" do
-      peeps = Peeps.new
-      expect(peeps.all.first).to eq "This is a peep!"
+      setup_test_database
+      add_row_to_test_database
+      expect(Peeps.all.first).to eq "This is a peep!"
     end
   end
 end
