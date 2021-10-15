8584c377014903c18e8e3dd0bb0f16e43c1b13a8
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-15 11:00:38 +0100

    refactor(peep): extract sql queries into constants

--- lib/peep.rb
+++ lib/peep.rb
@@ -2,6 +2,15 @@ require 'date'
 require_relative 'peep_factory'
 
 class Peep
+  FILTER_QUERY = 
+  "SELECT * FROM peeps WHERE UPPER(message) LIKE $1
+  ORDER BY date DESC, id DESC;".freeze
+  INSERT_NO_DATE_QUERY = 
+  "INSERT INTO peeps(message) VALUES($1) RETURNING id, message, date".freeze
+  INSERT_QUERY = 
+  "INSERT INTO peeps(message, date) VALUES($1, $2) RETURNING id, message, date".freeze
+  ALL_PEEPS_QUERY = "SELECT * FROM peeps ORDER BY date DESC, id DESC;".freeze
+
   def initialize(id:, message:, date:)
     @id = id
     @message = message
@@ -9,7 +18,7 @@ class Peep
   end
   
   def self.all
-    response = DatabaseConnection.query("SELECT * FROM peeps ORDER BY date DESC, id DESC;")
+    response = DatabaseConnection.query(ALL_PEEPS_QUERY)
     response.map do
       |peep| PeepFactory.wrap_peep_response_in_object(peep)
     end
@@ -18,11 +27,10 @@ class Peep
   def self.create(message, date = nil)
     if date.nil?
       response = DatabaseConnection.query(
-        "INSERT INTO peeps(message) VALUES($1) RETURNING id, message, date", [message])
+        INSERT_NO_DATE_QUERY, [message])
     else
       response = DatabaseConnection.query(
-        "INSERT INTO peeps(message, date) VALUES($1, $2) RETURNING id, message, date",
-        [message, date])
+        INSERT_QUERY, [message, date])
     end
     PeepFactory.wrap_peep_response_in_object(response.first)
   end
@@ -31,8 +39,7 @@ class Peep
     # upcase to match message case-insensitive
     formatted_message = "%#{message.upcase}%"
     response = DatabaseConnection.query(
-      "SELECT * FROM peeps WHERE UPPER(message) LIKE $1
-      ORDER BY date DESC, id DESC;", [formatted_message])
+      FILTER_QUERY, [formatted_message])
     response.map { |peep| PeepFactory.wrap_peep_response_in_object(peep) }
   end
 
