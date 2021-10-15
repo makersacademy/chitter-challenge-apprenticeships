0f7974a289b5e28cdf33cd895a3d35a296fc7537
Anamaria-Mocanu <Annamaariaaa@yahoo.com>
2021-10-15 18:10:16 +0100

    Refactored the tests

--- spec/chitter_spec.rb
+++ spec/chitter_spec.rb
@@ -1,20 +1,14 @@
 require "chitter_messaging"
 describe Chitter do
   describe "#all" do
-    it "returns all the messages" do
-      connection = PG.connect(dbname: 'chitter_test')
-      connection.exec("INSERT INTO peeps (message) values ('This is a peep!');")
-
- 
-      messages = ChitterMessage.all
- 
-      expect(messages).to eq (["This is a peep!"])
+    it "returns all the messages" do    
+      ChitterMessage.post("This is a peep!")
+      expect(ChitterMessage.all).to eq (["This is a peep!"])
     end
   end
 
   describe "#post" do
     it "adds a new message to the database" do
-      connection = PG.connect(dbname: 'chitter_test')
       ChitterMessage.post("Hello world")
       expect(ChitterMessage.all).to include "Hello world"
     end
--- spec/features/showing_all_spec.rb
+++ spec/features/showing_all_spec.rb
@@ -1,7 +1,7 @@
 feature "show all the messages" do
   scenario "shows all the messages" do
-    connection = PG.connect(dbname: 'chitter_test')
-    connection.exec("INSERT INTO peeps (message) values ('This is a peep!');") 
+    ChitterMessage.post("This is a peep!")
+
     visit "/messages" 
     expect(page).to have_content "This is a peep!"
   end
