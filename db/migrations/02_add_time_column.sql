754447156feea4d3d4b9d7311de0461e8fc57740
Matthew Read <matthewreadbass@hotmail.co.uk>
2021-10-15 15:10:04 +0100

    updated migrations to include setup instructions for date and time columns

--- db/migrations/02_add_time_column.sql
+++ db/migrations/02_add_time_column.sql
@@ -1,3 +1,4 @@
 In psql bookmark_manager & bookmark_manager_test databses
 
-ALTER TABLE peeps ADD COLUMN time DATE;
+ALTER TABLE peeps ADD COLUMN date DATE;
+ALTER TABLE peeps ADD COLUMN time TIME;
