43e1cf488c1e95373c9e2ebc417001967c9d1c66
Patrick Chan <q_p18@hotmail.com>
2021-10-15 15:18:41 +0100

    Connect to chitter_test db, insert values so to be able to display all messages in unit testing

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -3,6 +3,12 @@ require 'peep'
 describe Peep do
   describe '.all' do
     it 'returns all peeps' do
+      connection = PG.connect(dbname: 'chitter_test')
+
+      connection.exec("INSERT INTO peeps (message) VALUES('message 1');")
+      connection.exec("INSERT INTO peeps (message) VALUES('message 2');")
+      connection.exec("INSERT INTO peeps (message) VALUES('message 3');")
+
       peeps = Peep.all
 
       expect(peeps).to include('message 1')
