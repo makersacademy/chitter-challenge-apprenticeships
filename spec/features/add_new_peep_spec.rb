a356a76e6f8790717c34d542c5c986dfafbac0ea
Patrick Chan <q_p18@hotmail.com>
2021-10-15 15:57:29 +0100

    Fix typo

--- spec/features/add_new_peep_spec.rb
+++ spec/features/add_new_peep_spec.rb
@@ -4,6 +4,6 @@ feature 'Add a new peep' do
     fill_in('message', with: 'new message 1')
     click_button('Submit')
 
-    expect(page).to have 'new message 1'
+    expect(page).to have_content 'new message 1'
   end
 end
