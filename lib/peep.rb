ade68855c59ba750860215d646cda4709da14b61
Daniel <daniel.szabo09@gmail.com>
2021-10-16 00:06:21 +0100

    database secured

--- lib/peep.rb
+++ lib/peep.rb
@@ -33,7 +33,9 @@ class Peep
 
     current_time = Time.now
     time = current_time.strftime("%F %T")
-    result = connection.exec("INSERT INTO peeps (message, time) VALUES('#{message}', '#{time}') RETURNING id, message, time;")
+    result = connection.exec_params(
+      "INSERT INTO peeps (message, time) VALUES($1, $2) RETURNING id, message, time;",[message, time]
+    )
     Peep.new(id: result[0]['id'], message: result[0]['message'], time: result[0]['time'])
   end
 end
