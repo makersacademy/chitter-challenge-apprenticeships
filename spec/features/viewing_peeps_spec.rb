b3c52c079986f4218f830773141bfa5751a6f0b3
Patrick Chan <q_p18@hotmail.com>
2021-10-15 15:27:33 +0100

    Connect to chitter_test, insert values in feature testing

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -1,5 +1,11 @@
 feature 'See all messages in a browser' do
   scenario 'To see what people are doing, I want to see all the messages' do
+    connection = PG.connect(dbname: 'chitter_test')
+
+    connection.exec("INSERT INTO peeps (message) VALUES('message 1');")
+    connection.exec("INSERT INTO peeps (message) VALUES('message 2');")
+    connection.exec("INSERT INTO peeps (message) VALUES('message 3');")
+
     visit('/peeps')
     expect(page).to have_content 'message 1'
     expect(page).to have_content 'message 2'
