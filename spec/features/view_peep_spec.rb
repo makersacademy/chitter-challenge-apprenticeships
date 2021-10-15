182f59caffc5452e54f0eaf743abd72df4ec88d3
Matthew Read <matthewreadbass@hotmail.co.uk>
2021-10-15 14:49:58 +0100

    added passing feature test for .add & .view

--- spec/features/view_peep_spec.rb
+++ spec/features/view_peep_spec.rb
@@ -1,8 +1,12 @@
-# require "pg"
+require "pg"
 
-# feature "view peeps" do
-#   scenario "shows user a list of all peeps in a browser" do
-#     connection = PG.connect(dbname: "chitter_test")
-#     visit("/view")
-#   end
-# end
+feature "view peeps" do
+  scenario "shows user a list of all peeps in a browser" do
+    connection = PG.connect(dbname: "chitter_test")
+    visit("/add")
+    fill_in :message, with: "He talks in maths"
+    click_button "Peep"
+    visit "/view"
+    expect(page).to have_content("He talks in maths")
+  end
+end
