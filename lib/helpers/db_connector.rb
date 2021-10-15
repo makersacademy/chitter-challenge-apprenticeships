45af88dcb126a9c5b8247e69ebd218caa21f4d39
Tom Baxter <thomas99baxter@gmail.com>
2021-10-15 11:25:05 +0100

    rubocop + add DB helper

--- lib/helpers/db_connector.rb
+++ lib/helpers/db_connector.rb
@@ -1,2 +1,4 @@
-# database_name = ENV['RACK_ENV'] == 'development' ? 'chitter' : 'chitter_test'
-# con = PG.connect dbname: database_name, user: ENV['USER']
+def db_connection
+  database_name = ENV['RACK_ENV'] == 'development' ? 'chitter' : 'chitter_test'
+  con = PG.connect dbname: database_name, user: ENV['USER']
+end
--- lib/peep_accessor.rb
+++ lib/peep_accessor.rb
@@ -1,13 +1,12 @@
 require 'peep'
+require 'helpers/db_connector'
 class PeepAccessor
   def initialize; end
 
   def self.all
     peeps_arr = []
-    database_name = 'chitter_test'
-    con = PG.connect dbname: database_name, user: ENV['USER']
-
-    rs = con.exec 'SELECT * FROM peeps'
+  
+    rs = db_connection.exec 'SELECT * FROM peeps'
 
     rs.each do |row|
       peeps_arr << Peep.new(row)
@@ -18,8 +17,5 @@ class PeepAccessor
   rescue PG::Error => e
     puts e.message
   # :nocov:
-  ensure
-    rs&.clear
-    con&.close
   end
 end
