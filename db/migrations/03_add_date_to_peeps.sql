8b685e110971b3ede63bb92e326b11eccfeb5ba1
Patrick Chan <q_p18@hotmail.com>
2021-10-17 15:47:28 +0100

    Add date column

--- db/migrations/03_add_date_to_peeps.sql
+++ db/migrations/03_add_date_to_peeps.sql
@@ -1,3 +1,5 @@
-ALTER TABLE peeps ADD COLUMN date DATE NOT NULL DEFAULT CURRENT_DATE;
+ALTER TABLE peeps ADD COLUMN date DATE NOT NULL;
 
+-- Below allows Postgres to auto-generate a date but I don't know how to retrieve data
+-- ALTER TABLE peeps ADD COLUMN date DATE NOT NULL DEFAULT CURRENT_DATE;
 -- https://www.postgresqltutorial.com/postgresql-date/
