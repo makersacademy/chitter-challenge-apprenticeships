02b76142c5ff40b2237bc25bc5797af67a7f71c3
parsa <parsamesg@gmail.com>
2021-10-17 11:55:44 +0100

    add test for viewing peeps

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,4 +3,9 @@ feature "Viewing home page" do
     visit("/")
     expect(page).to have_content "chitter"
   end
+
+  scenario "viewing latest peeps on home page" do
+    visit("/")
+    expect(page).to have_content("This is a peep!")
+  end
 end
