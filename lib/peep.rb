f84705ee05f10a254af22e6b7731b878d5675742
Maisha <maisha-1998@hotmail.co.uk>
2021-10-17 09:17:06 +0100

    fixed unit tests

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,7 +1,14 @@
+  require 'pg'
+
 class Peep
-    def self.see_peeps
-      [
-        'This is a peep!',
-       ]
-    end
+  def self.see_peeps
+    if ENV['ENVIRONMENT'] == 'test'
+        connection = PG.connect(dbname: 'chitter_test')
+    else 
+        connection = PG.connect(dbname: 'chitter')
+    end 
+    
+    result = connection.exec("SELECT * FROM peeps;")
+    result.map { |peep| peep['message'] }
   end
+end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -3,6 +3,10 @@ require 'peep'
 describe Peep do
   describe '.see_peeps' do
     it 'returns all peeps' do
+      connection = PG.connect(dbname: 'chitter_test')
+
+      connection.exec("INSERT INTO peeps (messages) VALUES ('This is a peep!')") 
+
       peeps = Peep.see_peeps
 
       expect(peeps).to include('This is a peep!')
