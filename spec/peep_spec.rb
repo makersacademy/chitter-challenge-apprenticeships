30b0b3118e90bab78c0381ac72c16b9886f26b34
Patrick Chan <q_p18@hotmail.com>
2021-10-17 14:31:02 +0100

    Unit test .all and .add. Require database_helpers to use persisted_data(id:)

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,23 +1,32 @@
 require 'peep'
+require 'database_helpers'
 
 describe Peep do
   describe '.all' do
     it 'returns all peeps' do
-      Peep.add(message: 'message 123')
-      Peep.add(message: 'message 321')
+      peep = Peep.add(name: 'Peter', message: 'message 123')
+      Peep.add(name: 'Joey', message: 'message 321')
 
       peeps = Peep.all
 
-      expect(peeps).to include('message 123')
-      expect(peeps).to include('message 321')
+      expect(peeps.length).to eq 2
+      expect(peeps.first).to be_a Peep
+      expect(peeps.first.id).to eq peep.id
+      expect(peeps.first.name).to eq 'Peter'
+      expect(peeps.first.message).to eq 'message 123'
     end
   end
 
   describe '.add' do
     it 'adds a peep to the database' do
-      Peep.add(message: 'message ABC')
+      peep = Peep.add(name: 'Patrick', message: 'message ABC123')
+      p peep
+      persisted_data = persisted_data(id: peep.id)
 
-      expect(Peep.all).to include('message ABC')
+      expect(peep).to be_a Peep
+      expect(peep.id).to eq persisted_data['id']
+      expect(peep.message).to eq 'message ABC123'
+      expect(peep.name).to eq 'Patrick'
     end
   end
 end
