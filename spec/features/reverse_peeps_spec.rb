bd740483e23b759a0eb15eb9b2e490202d57cfe8
Troy Snowden <troybradsnowden@gmail.com>
2021-10-17 13:41:27 +0100

    Try and add feature test for reversing of chronological order

--- spec/features/reverse_peeps_spec.rb
+++ spec/features/reverse_peeps_spec.rb
@@ -1,7 +1,12 @@
 feature 'Reversing chronological order of peeps' do
-  scenario 'visiting the index page and reversing order of peeps' do
+  scenario 'reversing order of peeps' do
     visit('/')
+    click_button "Add peep"
+    fill_in("message", with: "New message added by feature test!")
+    click_button "Submit peep"
+    expect('This is a peep!').to appear_before('New message added by feature test!')
     click_button("Reverse Chronological Order")
-    # TODO: how to assert on order reversing?
+    ## This assertion won't work even though I have manually tested the reversing of order
+    # expect('This is a peep!').to_not appear_before('New message added by feature test!')
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -135,4 +135,11 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+## Custom RSpec matcher to check whether content appears before other content
+RSpec::Matchers.define :appear_before do |later_content|
+  match do |earlier_content|
+    page.body.index(earlier_content) < page.body.index(later_content)
+  end
+end
 end
