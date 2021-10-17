9b2ea251858d5466822f2cd3c0ba8f6ecbf0ef78
Patrick Chan <q_p18@hotmail.com>
2021-10-17 14:33:04 +0100

    Feature test fill_in name

--- spec/features/add_new_peep_spec.rb
+++ spec/features/add_new_peep_spec.rb
@@ -1,9 +1,11 @@
 feature 'Add a new peep' do
   scenario 'To post a peep so to update people of my status' do
     visit('/peeps/new')
+    fill_in('name', with: 'Patrick')
     fill_in('message', with: 'new message 1')
     click_button('Submit')
 
+    expect(page).to have_content 'Patrick'
     expect(page).to have_content 'new message 1'
   end
 end
