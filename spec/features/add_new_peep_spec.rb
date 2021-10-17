5599f361576385903d363d540b9d5066c0cea7b2
Patrick Chan <q_p18@hotmail.com>
2021-10-17 15:52:27 +0100

    Feature test to pass in date as an argument

--- spec/features/add_new_peep_spec.rb
+++ spec/features/add_new_peep_spec.rb
@@ -3,9 +3,11 @@ feature 'Add a new peep' do
     visit('/peeps/new')
     fill_in('name', with: 'Patrick')
     fill_in('message', with: 'new message 1')
+    fill_in('date', with: '2021-10-17')
     click_button('Submit')
 
     expect(page).to have_content 'Patrick'
     expect(page).to have_content 'new message 1'
+    expect(page).to have_content '2021-10-17'
   end
 end
--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -1,15 +1,20 @@
 feature 'See all messages in a browser' do
   scenario 'To see what people are doing, I want to see all the messages' do
-    Peep.add(name: 'Patrick', message: 'message 1')
-    Peep.add(name: 'Jon', message: 'message 2')
-    Peep.add(name: 'Mary', message: 'message 3')
+    Peep.add(name: 'Patrick', message: 'message 1', date: '2021-10-17')
+    Peep.add(name: 'Jon', message: 'message 2', date: '2021-10-18')
+    Peep.add(name: 'Mary', message: 'message 3', date: '2021-10-19')
 
     visit('/peeps')
     expect(page).to have_content 'Patrick'
-    expect(page).to have_content 'Jon'
-    expect(page).to have_content 'Mary'
     expect(page).to have_content 'message 1'
+    expect(page).to have_content '2021-10-17'
+
+    expect(page).to have_content 'Jon'
     expect(page).to have_content 'message 2'
+    expect(page).to have_content '2021-10-18'
+
+    expect(page).to have_content 'Mary'
     expect(page).to have_content 'message 3'
+    expect(page).to have_content '2021-10-19'
   end
 end
