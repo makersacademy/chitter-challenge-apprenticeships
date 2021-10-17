dfb14f34db978beda17cf4368ca8b146982c7745
parsa <parsamesg@gmail.com>
2021-10-17 17:32:43 +0100

    refactor to use create method

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -3,16 +3,15 @@ require "peep"
 describe Peep do
   describe "#all" do
     it "returns a list of peeps" do
-      db = PG.connect(dbname: "chitter_test")
-      db.exec("INSERT INTO peeps (message) VALUES ('This is a peep!');")
+      Peep.create("This is a peep!")
 
       expect(Peep.all).to include("This is a peep!")
     end
   end
   describe "#create" do
     it "adds a new peep to database" do
-      Peep.create("This is a new peep!")
-      expect(Peep.all).to include("This is a new peep!")
+      Peep.create("This is a peep!")
+      expect(Peep.all).to include("This is a peep!")
     end
   end
 end
