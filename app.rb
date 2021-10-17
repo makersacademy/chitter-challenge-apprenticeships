fb1684d78b33aae3392728d0631ee6a474a87e6b
parsa <parsamesg@gmail.com>
2021-10-17 16:29:52 +0100

    add sinatra reloader

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 require "sinatra/base"
+require "sinatra/reloader"
 require "./lib/peep"
 
 class Chitter < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
   get "/test" do
     "Test page"
   end
