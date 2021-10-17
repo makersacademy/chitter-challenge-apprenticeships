309ab3584371a834a31ba51953d450f3e5ec36f7
Patrick Chan <q_p18@hotmail.com>
2021-10-17 14:34:37 +0100

    Feature test viewing_peeps using Peep.add

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -1,10 +1,13 @@
 feature 'See all messages in a browser' do
   scenario 'To see what people are doing, I want to see all the messages' do
-    Peep.add(message: 'message 1')
-    Peep.add(message: 'message 2')
-    Peep.add(message: 'message 3')
+    Peep.add(name: 'Patrick', message: 'message 1')
+    Peep.add(name: 'Jon', message: 'message 2')
+    Peep.add(name: 'Mary', message: 'message 3')
 
     visit('/peeps')
+    expect(page).to have_content 'Patrick'
+    expect(page).to have_content 'Jon'
+    expect(page).to have_content 'Mary'
     expect(page).to have_content 'message 1'
     expect(page).to have_content 'message 2'
     expect(page).to have_content 'message 3'
