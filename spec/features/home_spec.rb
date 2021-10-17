754fd853ccebe25f694442c08933dd1dad88168a
parsa <parsamesg@gmail.com>
2021-10-17 19:01:06 +0100

    add test for user posting a peep

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -4,11 +4,19 @@ feature "Viewing home page" do
     expect(page).to have_content "chitter"
   end
 
-  scenario "viewing latest peeps on home page" do
+  scenario "user viewing latest peeps on home page" do
     db = PG.connect(dbname: "chitter_test")
     db.exec("INSERT INTO peeps (message) VALUES ('This is a peep!');")
 
     visit("/")
     expect(page).to have_content("This is a peep!")
+  end
+
+  scenario "user posting a new peep via a form" do
+    db = PG.connect(dbname: "chitter_test")
+    visit("/")
+    fill_in :new_peep, with: "Eating right now!"
+    click_button("post")
+    expect(page).to have_content("Eating right now!")
   end
 end
