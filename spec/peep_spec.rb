4e8639536c03a0d18668f14ea03892f4f7a9a321
Patrick Chan <q_p18@hotmail.com>
2021-10-17 15:53:00 +0100

    Unit test to pass in date as an argument

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -4,8 +4,8 @@ require 'database_helpers'
 describe Peep do
   describe '.all' do
     it 'returns all peeps' do
-      peep = Peep.add(name: 'Peter', message: 'message 123')
-      Peep.add(name: 'Joey', message: 'message 321')
+      peep = Peep.add(name: 'Peter', message: 'message 123', date: '2021-10-17')
+      Peep.add(name: 'Joey', message: 'message 321', date: '2021-10-17')
 
       peeps = Peep.all
 
@@ -14,19 +14,20 @@ describe Peep do
       expect(peeps.first.id).to eq peep.id
       expect(peeps.first.name).to eq 'Peter'
       expect(peeps.first.message).to eq 'message 123'
+      expect(peeps.first.date).to eq '2021-10-17'
     end
   end
 
   describe '.add' do
     it 'adds a peep to the database' do
-      peep = Peep.add(name: 'Patrick', message: 'message ABC123')
-      p peep
+      peep = Peep.add(name: 'Patrick', message: 'message ABC123', date: '2021-10-17')
       persisted_data = persisted_data(id: peep.id)
 
       expect(peep).to be_a Peep
       expect(peep.id).to eq persisted_data['id']
       expect(peep.message).to eq 'message ABC123'
       expect(peep.name).to eq 'Patrick'
+      expect(peep.date).to eq '2021-10-17'
     end
   end
 end
