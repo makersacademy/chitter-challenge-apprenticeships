32ff288da0f2c524674ec3da4cd157d2332e390e
ScottDonovan <spdonovan@hotmail.co.uk>
2021-11-04 12:22:22 +0000

    User stories diagram and first feature test for message class - Green

--- app.rb
+++ app.rb
@@ -5,5 +5,9 @@ class Chitter < Sinatra::Base
     'Test page'
   end
 
+  get '/message' do
+
+  end
+
   run! if app_file == $0
 end
---
+++ spec/features/messages_page_spec.rb
@@ -0,0 +1,8 @@
+require 'pg'
+require 'message'
+
+feature 'Opens a message page' do
+  scenario 'Visits messages' do
+    
+  end
+end
