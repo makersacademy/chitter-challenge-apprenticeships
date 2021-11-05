2d0052bec99ff1f040904da1b9982a30c3cec0ff
PatrickBarsch <PatrickGB@gmx.de>
2021-11-05 14:18:11 +0100

    Refactored test for showing the peep date using the helper method

--- spec/features/show_peep_date_spec.rb
+++ spec/features/show_peep_date_spec.rb
@@ -1,14 +1,8 @@
-require 'timecop'
-
 feature 'Show peep date' do
-  before do
-    test_time = Time.local(2021, 11, 5, 10, 5, 8)
-    Timecop.freeze(test_time)
-  end
   scenario 'A user can see, when a peep was added' do
     visit '/messages'
-    fill_in 'new_peep', with: 'Databases are really hard!'
-    click_button('Peep!')
+    peep = 'Databases are really hard!'
+    add_peep(peep, Time.local(2021, 11, 5, 10, 5, 8))
     expect(page).to have_current_path('/messages')
     expect(page).to have_content('Databases are really hard!')
     expect(page).to have_content('2021-11-05 10:05:08')
