3c71878fa557cb7100f309b0f565a2bf556c58c1
Dean Smythe <dean@teamtwisted.co.uk>
2021-11-05 11:19:38 +0000

    Connected to database

--- lib/peeps.rb
+++ lib/peeps.rb
@@ -3,7 +3,10 @@ require 'pg'
 class Peeps
 
 def self.all
-  return ['This is a peep!']
+  # return ['This is a peep!']
+  conn=PG.connect( dbname: 'chitter')
+  result = conn.exec('SELECT * FROM peeps;')
+  result.map {|peep| peep['message']}
 end
 
 end
