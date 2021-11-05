f95f74c979276b7ddbe56973284ce6ecd816fe2e
PatrickBarsch <PatrickGB@gmx.de>
2021-11-05 09:15:27 +0100

    Refactored by moving logic for creating a peep into the Peep class

--- lib/peep.rb
+++ lib/peep.rb
@@ -12,4 +12,14 @@ class Peep
     all_peeps.map { |peep| peep['message'] }
   end
 
+  def self.create(new_peep)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'chitter_test')
+    else
+      connection = PG.connect(dbname: 'chitter')
+    end
+    query = "INSERT INTO peeps (message) VALUES($1) RETURNING message;"
+    connection.exec_params(query, [new_peep])
+  end
+
 end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -11,4 +11,10 @@ describe Peep do
       expect(all_peeps.last).to eq('This is a peep!')
     end
   end
+  describe '.create' do
+    it 'Adds a peep to the database' do
+      test_peep = Peep.create('This is only a test')
+      expect(test_peep.first['message']).to eq 'This is only a test'
+    end
+  end
 end
