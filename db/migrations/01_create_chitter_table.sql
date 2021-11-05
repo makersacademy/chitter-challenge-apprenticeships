ad6b555240751962eaabc3b391294683d92f0cc0
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-11-05 13:15:42 +0000

    unit test for viewing peeps

--- db/migrations/01_create_chitter_table.sql
+++ db/migrations/01_create_chitter_table.sql
@@ -1,1 +1,2 @@
 CREATE TABLE peeps(id SERIAL PRIMARY KEY, message VARCHAR(60));
+INSERT INTO peeps (message) values ('This is a peep!');
---
+++ spec/peeps_spec.rb
@@ -0,0 +1,9 @@
+require 'peeps'
+
+describe Peeps do
+  describe '.all' do
+    it 'returns a list of peeps' do
+      
+    end
+  end
+end
