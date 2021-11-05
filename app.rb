fa0ddb1dc62fed1366ece76ed9cdef6fa0a8b5d6
PatrickBarsch <PatrickGB@gmx.de>
2021-11-05 08:14:51 +0100

    User story 2 - User can add a peep

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'pg'
 
 class Chitter < Sinatra::Base
   get '/test' do
@@ -6,7 +7,25 @@ class Chitter < Sinatra::Base
   end
 
   get '/messages' do
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'chitter_test')
+    else
+      connection = PG.connect(dbname: 'chitter')
+    end
+    all_peeps = connection.exec("SELECT * FROM peeps")
+    @all_peeps = all_peeps.map { |peep| peep['message'] }
     erb :messages
+  end
+
+  post '/messages/peep' do
+    new_peep = params[:new_peep]
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'chitter_test')
+    else
+      connection = PG.connect(dbname: 'chitter')
+    end
+    connection.exec_params("INSERT INTO peeps (message) VALUES($1);", [new_peep])
+    redirect '/messages'
   end
 
   run! if app_file == $0
