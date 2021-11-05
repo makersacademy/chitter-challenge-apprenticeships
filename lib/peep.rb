fddb2f30f89a744563bcb43f1d14cfada2c68123
PatrickBarsch <PatrickGB@gmx.de>
2021-11-05 09:17:14 +0100

    Refactored by moving logic for connecting to database into a separate method

--- lib/peep.rb
+++ lib/peep.rb
@@ -2,22 +2,22 @@ require 'pg'
 
 class Peep
 
-  def self.all 
+  def self.connect
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'chitter_test')
     else
       connection = PG.connect(dbname: 'chitter')
     end
+  end
+
+  def self.all 
+    connection = Peep.connect
     all_peeps = connection.exec("SELECT * FROM peeps")
     all_peeps.map { |peep| peep['message'] }
   end
 
   def self.create(new_peep)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
+    connection = Peep.connect
     query = "INSERT INTO peeps (message) VALUES($1) RETURNING message;"
     connection.exec_params(query, [new_peep])
   end
