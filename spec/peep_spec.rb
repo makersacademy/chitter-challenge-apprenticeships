2246631d836a7134bd2cfeb20c64ac5fedb37b1d
PatrickBarsch <PatrickGB@gmx.de>
2021-11-05 09:32:19 +0100

    Refactored Peep.all test using Peep.create

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -3,12 +3,12 @@ require 'peep'
 describe Peep do 
   describe '.all' do
     it 'Returns all available peeps' do
-      add_row_to_test_database
-      add_row_to_test_database
-      add_row_to_test_database
+      Peep.create('This is only a test!')
+      Peep.create('This is another test!')
+      Peep.create('This is the final test!')
       all_peeps = Peep.all
       expect(all_peeps.length).to eq 3
-      expect(all_peeps.last).to eq('This is a peep!')
+      expect(all_peeps.last).to eq('This is the final test!')
     end
   end
   describe '.create' do
