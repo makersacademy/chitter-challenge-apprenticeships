56a0445c965f63c15e394c0ade46ab7be3a0e7ba
Jenaro Garcia <jjegarcia@hotmail.com>
2021-11-06 08:05:12 +0000

    added missing tests in peeps sorting and filtering

--- spec/peeps_spec.rb
+++ spec/peeps_spec.rb
@@ -1,6 +1,9 @@
 require 'peeps'
 require 'peep'
 TEST_PEEP = Peep.new("test_message")
+PEEP_LAST = Peep.new("test PEEP", "sept112")
+PEEP_NEW = Peep.new("LAST PEEP", "sept11")
+
 describe Peeps do
   it 'empty should report error' do
     expect { subject.list }.to raise_error 'Empty List'
@@ -9,5 +12,15 @@ describe Peeps do
     subject.add(TEST_PEEP)
     expect(subject.last_record).to eq TEST_PEEP
   end
-
+  it 'should sort descending' do
+    subject.add(TEST_PEEP)
+    subject.add(PEEP_NEW)
+    expect(subject.sort_descending[0]).to eq PEEP_NEW
+  end
+  it 'should filter by keyword' do
+    subject.add(TEST_PEEP)
+    subject.add(PEEP_NEW)
+    subject.add(PEEP_LAST)
+    expect(subject.filter("test")).to eq [PEEP_LAST, TEST_PEEP]
+  end
 end
