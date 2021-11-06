e6bfe16538dcf418594b8062776cfae00c431e4f
MarcBrosch <marc.brosch@ford.com>
2021-11-06 13:41:05 +0100

    User post will be safed into database

--- app.rb
+++ app.rb
@@ -1,13 +1,23 @@
 require 'sinatra/base'
+require 'pg'
+require './lib/messages'
 
 class Chitter < Sinatra::Base
 
   get '/messages' do
+    @messages = Messages.all
     erb :messages
   end
 
   post '/messages' do
     @peep = params[:peep]
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'chitter_test')
+    else
+      connection = PG.connect(dbname: 'chitter')
+    end
+    connection.exec("INSERT INTO peeps (message) VALUES('#{@peep}')")
+    redirect '/messages'
   end
 
   get '/post_message' do 
