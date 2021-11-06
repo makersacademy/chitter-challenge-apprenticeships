b6911df4ce4fbd43501bfd8df7deeb72ecb1a1dc
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-11-06 21:04:32 +0000

    Peep page class connected to database

---
+++ lib/database_connection.rb
@@ -0,0 +1,16 @@
+require 'pg'
+
+class DatabaseConnection
+  def self.setup
+    @connection =
+      if ENV['ENVIRONMENT'] == 'test'
+        PG.connect(dbname: 'chitter_test')
+      else
+        PG.connect(dbname: 'chitter')
+      end
+  end
+
+  def self.query(sql)
+    @connection.exec(sql)
+  end
+end
