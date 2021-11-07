a5a19fe6edbf7c0a421935d5fa4f6a41be1272ec
Raul <rauldelahera@hotmail.com>
2021-11-07 20:11:05 +0000

    First user story completed

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -1,8 +1,13 @@
 feature 'Viewing peeps' do
   scenario 'A user can see the peeps' do
-    visit('/messages')
+    connection = PG.connect(dbname: 'chitter_test')
+      
+    connection.exec("INSERT INTO peeps (message) VALUES ('This is a peep!');")
+    connection.exec("INSERT INTO peeps (message) VALUES('This is another peep!');")
 
+    visit('/messages')
+    
     expect(page).to have_content "This is a peep!"
-    # expect(page).to have_content "This is another peep!"
+    expect(page).to have_content "This is another peep!"
   end
 end
--- spec/messages_spec.rb
+++ spec/messages_spec.rb
@@ -3,10 +3,15 @@ require 'messages'
 describe Messages do
   describe '.all' do
     it 'return all peeps' do
+      connection = PG.connect(dbname: 'chitter_test')
+      
+      connection.exec("INSERT INTO peeps (message) VALUES ('This is a peep!');")
+      connection.exec("INSERT INTO peeps (message) VALUES('This is another peep!');")
+
       peeps = Messages.all
 
       expect(peeps).to include('This is a peep!')
-      # expect(peeps).to include('This is another peep!')
+      expect(peeps).to include('This is another peep!')
     end
   end
 end
