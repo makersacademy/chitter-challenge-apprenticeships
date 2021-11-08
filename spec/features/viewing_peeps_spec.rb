a34da934dda7265c3774954128ffb6fbe16ae2bd
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2021-11-08 09:22:56 +0000

    Feature test for posting a peep

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -7,3 +7,13 @@ feature 'Viewing peeps' do
     expect(page).to have_content "Test peep!"
   end
 end
+
+feature 'Posting a peeps' do
+  scenario 'User can post a peep' do
+    Peep.create(message: "Test peep!")
+    visit('/peeps')
+    fill_in :message, with: 'Test peep!'
+    click_button 'Submit'
+    expect(page).to have_content "Test peep!"
+  end
+end
