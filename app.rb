c174eb46adb776eb686e0c710dde767b219c8619
Rebecca <Beckyrose200@aol.com>
2021-11-08 09:30:43 +0000

    All tests working

--- app.rb
+++ app.rb
@@ -6,20 +6,23 @@ class Chitter < Sinatra::Base
     'Test page'
   end
 
-get '/messages' do 
-  @peeps = Peeps.all
-  erb :messages
+  get '/messages' do 
+    @peeps = Peeps.all
+    erb :messages
+  end 
+
+  get '/posting' do
+    erb :post
+  end 
+
+  post '/posting' do
+    Peeps.create(message: params[:posting])
+    redirect '/messages'
+  end 
+
+  run! if app_file == $0
 end 
 
-get '/posting' do
-  erb :post
-end 
-
-post '/posting' do
- Peeps.create(message: params[:posting])
- redirect '/messages'
-end 
-
-run! if app_file == $0
-
-end 
+# As a Maker
+# So that I can see when people are doing things
+# I want to see the date the message was posted
