49584bcd76f7283fd1e51628b97e23578f544a84
uburger <ursburger@gmail.com>
2021-11-08 11:16:42 +0100

    User story 2 done

--- app.rb
+++ app.rb
@@ -11,5 +11,13 @@ class Chitter < Sinatra::Base
     erb :index
   end
 
+  get '/peeps/add' do
+    erb :message_new
+  end
+
+  post '/peeps/new' do
+    Message.add(message: params[:message])
+    redirect '/peeps'
+  end
   run! if app_file == $0
 end
