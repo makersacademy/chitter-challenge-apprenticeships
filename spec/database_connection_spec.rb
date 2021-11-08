617b95134dd96339289859e945e49c889a338118
PatrickBarsch <PatrickGB@gmx.de>
2021-11-08 08:10:57 +0100

    Made connection in DatabaseConnection persistent through saving it in an instance variable

--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -3,8 +3,14 @@ require 'database_connection'
 describe DatabaseConnection do
   describe '.setup' do
     it 'sets up a connection to a database through PG' do 
-      expect(PG).to receive(:connect).with(dbname: 'bookmark_manager_test')
-      DatabaseConnection.setup('bookmark_manager_test')
+      expect(PG).to receive(:connect).with(dbname: 'chitter_test')
+      DatabaseConnection.setup('chitter_test')
+    end
+  end
+  describe '.connection' do
+    it 'this connection is persistent' do
+      connection = DatabaseConnection.setup('chitter_test')
+      expect(DatabaseConnection.connection).to eq connection
     end
   end
 end
