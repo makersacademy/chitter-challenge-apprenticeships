af46d8d85bb19eaa0a87bc20577681e8bc380eb8
jyoung-gds <jonathan.young@digital.cabinet-office.gov.uk>
2022-01-21 11:55:22 +0000

    Separated tests for viewing messages and viewing dates

--- spec/features/view_all_peeps_spec.rb
+++ spec/features/view_all_peeps_spec.rb
@@ -2,8 +2,19 @@
 # So that I can see what people are doing
 # I want to see all the messages (peeps) in a browser
 
+# As a Maker
+# So that I can see when people are doing things
+# I want to see the date the message was posted
+
 feature "viewing all messages (peeps)" do
   scenario "view all peeps" do
+    visit('/peeps')
+
+    expect(page).to have_content("This is a test peep!")
+    expect(page).to have_content("This is another test peep!")
+  end
+
+  scenario "view the date the message was posted" do
     visit('/peeps')
     today_date = Time.now.strftime("%Y-%m-%d")
 
