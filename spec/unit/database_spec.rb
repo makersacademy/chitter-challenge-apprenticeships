2f96009870ffff7c907a49774d7bbbbb5462fe9c
Sam Pickering <sam.p@tuta.io>
2022-01-21 14:48:52 +0000

    fix database unit tests breaking database connection state

--- spec/unit/database_spec.rb
+++ spec/unit/database_spec.rb
@@ -3,14 +3,14 @@ require 'database'
 describe Database do
   describe '#connect' do
     it 'connects using PG.connect' do
-      expect(PG).to receive(:connect).with(dbname: 'database_name')
+      expect(PG).to receive(:connect).with(dbname: 'database_name').and_return(described_class.connection)
       described_class.connect('database_name')
     end
   end
 
   describe '#connection' do
     it 'returns the connection received from #connect' do
-      allow(PG).to receive(:connect).and_return(double)
+      allow(PG).to receive(:connect).and_return(described_class.connection)
       connection = described_class.connect('database_name')
       expect(described_class.connection).to eq(connection)
     end
@@ -18,8 +18,6 @@ describe Database do
 
   describe '#query' do
     it 'executes a query using exec_params' do
-      allow(PG).to receive(:connect).and_return(double)
-      described_class.connect('database_name')
       expect(described_class.connection).to receive(:exec_params).with('INSERT INTO table_name VALUES ($1);', [1])
       described_class.query('INSERT INTO table_name VALUES ($1);', [1])
     end
