fd45670dd148eb6402f7192230ac4616cdf86cf6
jyoung-gds <jonathan.young@digital.cabinet-office.gov.uk>
2022-01-21 15:56:48 +0000

    Refactored to apply DRY to database connections in peep.rb

--- lib/peep.rb
+++ lib/peep.rb
@@ -11,31 +11,24 @@ class Peep
   end
 
   def self.all
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-    results = connection.exec("SELECT * FROM peeps ORDER BY date DESC;")
+    results = connect.exec("SELECT * FROM peeps ORDER BY date DESC;")
     results.map { |peep| Peep.new(id: peep['id'], message: peep['message'], date: peep['date'])}
   end
 
   def self.create(message)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-    connection.exec("INSERT INTO peeps (message, date) values ('#{message}', 'NOW()');")
+    connect.exec("INSERT INTO peeps (message, date) values ('#{message}', 'NOW()');")
   end
 
   def self.filtered(keyword)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-    results = connection.exec("SELECT * FROM peeps WHERE LOWER(message) LIKE '%#{keyword.downcase}%' ORDER BY date DESC;")
+    results = connect.exec("SELECT * FROM peeps WHERE LOWER(message) LIKE '%#{keyword.downcase}%' ORDER BY date DESC;")
     results.map { |peep| Peep.new(id: peep['id'], message: peep['message'], date: peep['date'])}
+  end
+
+  def self.connect
+    if ENV['ENVIRONMENT'] == 'test'
+      PG.connect(dbname: 'chitter_test')
+    else
+      PG.connect(dbname: 'chitter')
+    end
   end
 end
