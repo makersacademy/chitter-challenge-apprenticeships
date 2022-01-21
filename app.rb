cf28970a7ef12aef5663c4bdc7c05a28d4f45642
Sam Pickering <sam.p@tuta.io>
2022-01-21 11:28:44 +0000

    pass viewing peep test

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require 'sinatra/base'
+require 'pg'
 
 class Chitter < Sinatra::Base
+  get '/' do
+    connection = PG.connect(dbname: 'chitter_test')
+    result = connection.exec('SELECT message FROM peeps;')
+    @peeps = result.map { |row| row['message'] }
+    erb :index
+  end
+
   get '/test' do
     'Test page'
   end
