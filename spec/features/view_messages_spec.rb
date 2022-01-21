becf4be6c4580ccfc06a9b9a244790735ef76346
code-to <Theresaokyere034@gmail.com>
2022-01-21 15:29:49 +0000

    refactoring view messages spec

--- spec/features/view_messages_spec.rb
+++ spec/features/view_messages_spec.rb
@@ -4,9 +4,9 @@ require 'pg'
 feature 'viewing messages' do
   scenario 'are on the page' do
     connection = PG.connect(dbname: 'chitter_test')
-    connection.exec("INSERT INTO peeps (message) VALUES ('Hi there');")
-    connection.exec("INSERT INTO peeps (message) VALUES('How are you today?');")
-
+    Message.create(message:'Hi there')
+    Message.create(message:'How are you today?')
+   
     visit '/messages'
     expect(page).to have_content('Hi there')
     expect(page).to have_content('How are you today?')
