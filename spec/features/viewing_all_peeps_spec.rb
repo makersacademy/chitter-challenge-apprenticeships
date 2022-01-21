fc0c3b03540c351d512fb7debe58d3cc87f44da2
OliviaMaxwell <olivia@maxwellnet.com>
2022-01-21 11:29:33 +0000

    Updated Test Environment to access PostgreSQL

--- spec/features/viewing_all_peeps_spec.rb
+++ spec/features/viewing_all_peeps_spec.rb
@@ -7,6 +7,12 @@ end
 
 feature 'Viewing all peeps' do
   scenario 'User can view all the peeps (messages)' do
+    connection = PG.connect(dbname: 'chitter_test')
+
+    connection.exec("INSERT INTO peeps (message) VALUES ('This is my first peep!');")
+    connection.exec("INSERT INTO peeps (message) VALUES ('This is my second peep!');")
+    connection.exec("INSERT INTO peeps (message) VALUES ('This is my third peep!');")
+    
     visit ('/peeps')
 
     expect(page).to have_content "This is my first peep!"
--- spec/unit/peeps_spec.rb
+++ spec/unit/peeps_spec.rb
@@ -4,6 +4,12 @@ describe Peep do
 
   describe '#all' do
     it 'lists all the peeps' do
+      connection = PG.connect(dbname: 'chitter_test')
+
+      connection.exec("INSERT INTO peeps (message) VALUES ('This is my first peep!');")
+      connection.exec("INSERT INTO peeps (message) VALUES ('This is my second peep!');")
+      connection.exec("INSERT INTO peeps (message) VALUES ('This is my third peep!');")
+
       peeps_list = Peep.all
 
       expect(peeps_list).to include "This is my first peep!"
