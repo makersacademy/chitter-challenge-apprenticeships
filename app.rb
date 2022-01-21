75b433d3d2864db17258636e1997371914cfe547
Susana <susanamncwabe@outlook.com>
2022-01-21 12:20:21 +0000

    first commit

--- app.rb
+++ app.rb
@@ -1,9 +1,23 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
 
 class Chitter < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+
   get '/test' do
     'Test page'
   end
+
+
+ get '/' do
+   erb(:index)
+end
+
+
+
 
   run! if app_file == $0
 end
