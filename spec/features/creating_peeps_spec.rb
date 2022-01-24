bb484c3102ae7cf1a47abcfd03073f9af7ad765a
code-to <Theresaokyere034@gmail.com>
2022-01-24 03:28:32 +0000

    added date to the feature test

--- spec/features/creating_peeps_spec.rb
+++ spec/features/creating_peeps_spec.rb
@@ -2,6 +2,7 @@ feature 'Adding a new message(peep)' do
   scenario 'A user can post a message(peep) to chitter' do
     visit('/messages/new')
     fill_in('message', with: 'a new post')
+    fill_in('date-posted', with: '2022-01-12T19:30')
     click_button('POST!')
     
     expect(page).to have_content 'a new post'
