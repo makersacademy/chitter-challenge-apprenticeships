3df7b12206fc0a6567dea63b5c5d6d77f439822c
Daniel Watts <d.watts15@outlook.com>
2022-02-11 12:48:56 +0000

    Refactored dummy insertion in viewing_messages_spec

--- spec/features/viewing_messages_spec.rb
+++ spec/features/viewing_messages_spec.rb
@@ -2,11 +2,9 @@ require 'pg'
 
 feature "viewing messages" do
   scenario "user can view all messages on the message board page" do
-    connection = PG.connect(dbname: 'chitter_test')
+    Message.create(name: "Bob", message: "Test Message 1")
+    Message.create(name: "Ron", message: "Test Message 2")
 
-    connection.exec("INSERT INTO peeps VALUES(1, 'Test Message 1', 'Bob');")
-    connection.exec("INSERT INTO peeps VALUES(2, 'Test Message 2', 'Ron');")
-    
     visit ('/messageboard')
 
     expect(page).to have_content "Bob - Test Message 1"
