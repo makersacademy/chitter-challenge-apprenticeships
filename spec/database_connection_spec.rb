f1a5b16bc47e32c276522ea26d4863ca1af3cc63
T Henebury <tiffsisstracker@gmail.com>
2022-02-11 12:29:49 +0000

    database connection persistent

--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -6,6 +6,11 @@ describe DatabaseConnection do
       expect(PG).to receive(:connect).with(dbname: 'chitter_test')
       DatabaseConnection.setup('chitter_test')
     end
+
+    it 'has persistent connection' do
+      connection = DatabaseConnection.setup('chitter_test')
+
+      expect(DatabaseConnection.connection).to eq connection
+    end
   end
 end
-
