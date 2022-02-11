3c13bbe8194aa407dcced10c2f9156153c19696c
David <dave_carroll82@hotmail.com>
2022-02-11 17:09:55 +0000

    adding test database

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,5 +1,13 @@
 class Peep
   def self.all
-  ["This is a peep!"]
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+
+    connection = PG.connect(dbname: 'chitter')
+    result = connection.exec('SELECT * FROM peeps')
+    result.map { |peep| peep ['message']}
   end
 end
