f2d9442f2c64b4ab93fec578f34c0b4bcca3ce45
Kyrsten <Kyrsten>
2022-02-11 11:40:21 +0000

    2nd test passed view messages hardcoded

--- app.rb
+++ app.rb
@@ -1,8 +1,20 @@
 require 'sinatra/base'
+# require 'sinatra/reloader'
 
 class Chitter < Sinatra::Base
+  # configure :development do
+  #   register Sinatra::Reloader
+  # end
+
   get '/test' do
     'Test page'
+  end
+
+  get '/view_messages' do
+    messages = ['This is a peep',
+                'and another peep'
+              ]
+    messages.join
   end
 
   run! if app_file == $0
