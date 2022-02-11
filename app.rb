76bbcce4c037bd07ad37e4b5be4073079dfa9481
Jaabir Yusuf <jaabir-mahmoud@hotmail.com>
2022-02-11 10:31:26 +0000

    Chitter basic web page and built file structure

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
-require 'sinatra/base'
+require 'sinatra'
+require "sinatra/reloader" if development?
+require_relative "./lib/chitter_message"
 
 class Chitter < Sinatra::Base
+  enable :sessions
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    erb :index
+  end
+
   get '/test' do
     'Test page'
   end
