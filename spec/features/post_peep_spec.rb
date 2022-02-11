dc6eef06f99141050422ba315e5de42c6dd05115
Will Diable <wtdiable@icloud.com>
2022-02-11 17:49:43 +0000

    Improved presentation of Peep

--- spec/features/post_peep_spec.rb
+++ spec/features/post_peep_spec.rb
@@ -3,7 +3,7 @@ feature 'Posting a peep' do
   scenario 'A user can post a peep on Chitter' do
     visit('/new')
     fill_in('message', with: 'Oh, dress yourself, my urchin one')
-    click_button('Submit')
+    click_button('Post your Peep!')
 
     expect(page).to have_content 'Oh, dress yourself, my urchin one'
   end
