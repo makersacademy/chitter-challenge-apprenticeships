4361ae0d7ac05b4e988f42cb38b8f366aeefc3ec
Oliver Cutting <oliver.cutting@gmail.com>
2022-02-11 16:27:30 +0000

    Added direct link button to newsfeed for filter

--- spec/features/filter_keyword_spec.rb
+++ spec/features/filter_keyword_spec.rb
@@ -1,4 +1,11 @@
 feature 'Filter keyword' do
+  scenario 'A user can navigate to the filter page using a button' do
+    visit('/newsfeed')
+    click_button('Filter')
+
+    expect(current_path).to eq('/newsfeed/filter')
+  end
+
   scenario 'A user can filter peeps by keyword' do
     visit('/newsfeed/filter')
     fill_in('filter', with: 'Filter')
