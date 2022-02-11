127359c08d914ecfd9e016f1eefe6dd9f4371c29
benfielding74 <billy.fadden@live.com>
2022-02-11 14:00:18 +0000

    implemented and passed tests using test database

--- spec/chitter_spec.rb
+++ spec/chitter_spec.rb
@@ -4,10 +4,16 @@ describe ChitterManager do
 
   describe '.all' do
     it 'returns a list of all messages' do
+      connection = PG.connect(dbname: 'chitter_test')
+
+      connection.exec("INSERT INTO peeps (message) VALUES ('Hey there it''s Friday');")
+      connection.exec("INSERT INTO peeps (message) VALUES ('Fri-Yay!');")
+
       peeps = ChitterManager.all
 
       expect(peeps).to include("Hey there it's Friday")
+      expect(peeps).to include("Fri-Yay!")
     end
   end
-  
+
 end
--- spec/features/test_page_spec.rb
+++ spec/features/test_page_spec.rb
@@ -1,6 +1,12 @@
 feature 'Viewing test page' do
   scenario 'visiting the test page' do
+    connection = PG.connect(dbname: 'chitter_test')
+
+    connection.exec("INSERT INTO peeps (message) VALUES ('Hey there it''s Friday');")
+    connection.exec("INSERT INTO peeps (message) VALUES ('Fri-Yay!');")
+  
     visit('/chitter')
     expect(page).to have_content "Hey there it's Friday"
+    expect(page).to have_content "Fri-Yay!"
   end
 end
