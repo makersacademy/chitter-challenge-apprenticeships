4c85c0f6e457bfc6c5089b53ea6be63cf54b4b02
Lauren Gray <laurenbethgrayx@gmail.com>
2022-02-14 09:19:54 +0000

    Final commit, second user story complete

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -3,4 +3,16 @@ feature 'Viewing Peeps' do
     visit('/')
     expect(page).to have_content "Chitter: Latest Peeps"
   end
+
+  scenario 'Visiting /bookmarks shows me all the peeps' do
+    connection = PG.connect(dbname: 'chitter_test')
+
+    # add test data
+    connection.exec("INSERT INTO peeps (message) VALUES ('This is a peep!');")
+    Peeps.create(message: "This is a peep!")
+
+    visit('/peeps')
+  end
+
+
 end
