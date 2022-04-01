b64d5c86bccfd1216b908a82e7a0421c8acbb337
hamolicious <kickfliptv8@gmail.com>
2022-04-01 13:01:00 +0100

    refactored to return User instance

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,16 +1,36 @@
 require_relative './databaseconnection'
 
 class User
+  attr_reader :id, :username, :password
+
+  # User object methods
+  def initialize(hash)
+    @id = hash.fetch('id')
+    @username = hash.fetch('username')
+    @password = hash.fetch('password')
+  end
+
+  def to_h
+    {
+      'id' => @id,
+      'username' => @username,
+      'password' => @password,
+    }
+  end
+
   # Static Database Methods
   def self.get_by_id(id)
-    DatabaseConnection.query("SELECT * FROM users WHERE id='#{id}'")
+    r = DatabaseConnection.query("SELECT * FROM users WHERE id='#{id}'")
+    User.new(r.to_a[0])
   end
 
   def self.get_by_username(username)
-    DatabaseConnection.query("SELECT * FROM users WHERE username='#{username}'")
+    r = DatabaseConnection.query("SELECT * FROM users WHERE username='#{username}'")
+    User.new(r.to_a[0])
   end
 
   def self.create(username, password)
     DatabaseConnection.query("INSERT INTO users(username, password) VALUES('#{username}', '#{password}')")
+    return get_by_username(username)
   end
 end
--- spec/user_model_spec.rb
+++ spec/user_model_spec.rb
@@ -5,27 +5,22 @@ describe User do
   context 'Testing Static Methods,' do
     specify 'should create a user' do
       r = User.create('test', 'password')
-      expect(r).to be_instance_of (PG::Result)
+      expect(r).to be_instance_of (User)
+      expect(r.username).to eq ('test')
+      expect(r.password).to eq ('password')
     end
 
     specify 'should get a user by id' do
-      User.create('test', 'password')
-      r = User.get_by_username('test')
-
-      expect(r).to be_instance_of (PG::Result)
-      expect(r[0]).to be_instance_of (Hash)
-
-      id = r[0]['id'] # variable id
-      expect(r.to_a).to eq [{ 'id' => id, 'username' => 'test', 'password' => 'password' }]
+      user = User.create('test', 'password')
+      r = User.get_by_id(user.id)
+      expect(r.to_h).to eq ({ 'id' => user.id, 'username' => 'test', 'password' => 'password' })
     end
 
     specify 'should get a user by username' do
-      User.create('test', 'password')
-      temp = User.get_by_username('test')
-      id = temp[0]['id'] # variable id
-      r = User.get_by_id(id)
+      user = User.create('test', 'password')
+      r = User.get_by_username(user.username)
 
-      expect(r.to_a).to eq [{ 'id' => id, 'username' => 'test', 'password' => 'password' }]
+      expect(r.to_h).to eq ({ 'id' => user.id, 'username' => 'test', 'password' => 'password' })
     end
   end
 end
