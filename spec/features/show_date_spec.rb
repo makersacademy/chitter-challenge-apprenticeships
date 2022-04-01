47ddf645ee1617f6566206ef8683af89d29bc319
Lucinda Curnow <lucinda.curnow@gmail.com>
2022-04-01 14:38:31 +0100

    Added feature test for displaying date of peep

---
+++ spec/features/show_date_spec.rb
@@ -0,0 +1,9 @@
+feature 'include date' do
+  scenario 'each peep displays the date when it was posted' do
+    visit('/view_peeps')
+    fill_in('message', with: 'Posted today')
+    click_button('PEEP!')
+    todays_date = Date.today
+    expect(page).to have_content todays_date
+  end
+end
