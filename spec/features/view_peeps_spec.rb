7948ca8036c7f8569e113fa8484a7ee72962acea
Laura Cabantous <laura.cabantous@gmail.com>
2022-04-01 12:58:33 +0100

    Set up testing environment

--- spec/features/view_peeps_spec.rb
+++ spec/features/view_peeps_spec.rb
@@ -1,8 +1,16 @@
 require './app'
+require 'pg'
 
 RSpec.describe "Peeps" do
     feature "view peeps" do
         scenario "user can view all peeps in their browser" do
+            connection = PG.connect(dbname: 'chitter_test')
+
+             # Add the test data
+            connection.exec("INSERT INTO peeps (message) VALUES ('Hello world!');")
+            connection.exec("INSERT INTO peeps (message) VALUES('Hi Chitter! This is my first peep!');")
+            connection.exec("INSERT INTO peeps (message) VALUES('This is a peep!');")
+
             visit('/chitter')
 
             expect(page).to have_content("Hello world!")
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -3,11 +3,18 @@ require 'peep'
 describe Peep do
   describe '.all' do
     it 'returns all peeps' do
-      peeps = Peep.all
+        connection = PG.connect(dbname: 'chitter_test')
 
-      expect(peeps).to include("Hello world!")
-      expect(peeps).to include("Hi Chitter! This is my first peep!")
-      expect(peeps).to include("This is a peep!")
+         # Add the test data
+        connection.exec("INSERT INTO peeps (message) VALUES ('Hello world!');")
+        connection.exec("INSERT INTO peeps (message) VALUES('Hi Chitter! This is my first peep!');")
+        connection.exec("INSERT INTO peeps (message) VALUES('This is a peep!');")
+
+        peeps = Peep.all
+
+        expect(peeps).to include("Hello world!")
+        expect(peeps).to include("Hi Chitter! This is my first peep!")
+        expect(peeps).to include("This is a peep!")
     end
   end
 end
