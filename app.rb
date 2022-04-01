29ca323f4d0a8cc606b507ad357c3240a8d9dd8c
William Marshall <w.marshall@elsevier.com>
2022-04-01 12:16:46 +0100

    Add post peep feature

--- app.rb
+++ app.rb
@@ -15,5 +15,14 @@ class Chitter < Sinatra::Base
     erb(:"peeps/index")
   end
 
+  get "/peeps/new" do
+    erb(:"peeps/new")
+  end
+
+  post "/peeps" do
+    Peep.create(message: params[:message])
+    redirect to("/peeps")
+  end 
+
   run! if app_file == $0
 end
