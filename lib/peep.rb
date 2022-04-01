87cc480fbd4816c7d404e71bb0177353cf3fba31
shannon white <shannonw-l@hotmail.co.uk>
2022-04-01 13:52:29 +0100

    user story 2

--- lib/peep.rb
+++ lib/peep.rb
@@ -18,7 +18,7 @@ class Peep
 
     result = connection.exec("SELECT * FROM peeps;")
     result.map do |peep|
-      Peep.new(id: peep['id'], message: peep['message'])
+    Peep.new(id: peep['id'], message: peep['message'])
     end
   end
 
@@ -28,7 +28,7 @@ class Peep
     else
       connection = PG.connect(dbname: 'chitter')
     end
-    
+
     result = connection.exec("INSERT INTO peeps (message) VALUES('#{message}') RETURNING id, message")
     Peep.new(id: result[0]['id'], message: result[0]['message'])
   end
