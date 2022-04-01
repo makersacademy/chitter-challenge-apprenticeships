0099a004f9e167431b708d4c7f70030320e8f2b9
Peter Hattyar <peter.hattyar@gmail.com>
2022-04-01 12:35:34 +0100

    /chitter main welcome page, model for database all content all test driven and implemented.

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'sinatra/base'
+require_relative './lib/little_chits'
 
 class Chitter < Sinatra::Base
   get '/test' do
     'Test page'
+  end
+
+  get '/chitter' do
+    @all_chits = Chits.all
+    erb(:index)
   end
 
   run! if app_file == $0
