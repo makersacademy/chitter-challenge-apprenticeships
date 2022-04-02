b47be5b50ba637b838ff6e36147e7b6cc7956847
stephaniesomade <stephaniesomadee@gmail.com>
2022-04-02 03:04:09 +0100

    Setting up the databases

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require 'sinatra/base'
 
 class Chitter < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
   get '/test' do
     'Test page'
+  end
+
+  get '/' do 
   end
 
   run! if app_file == $0
