f5d2c53042885352dddab04361ac43dcf722123e
jkgiwa <jkgiwa95@gmail.com>
2022-04-04 09:43:58 +0100

    added extras

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require 'sinatra/base'
+require 'pg'
 
 class Chitter < Sinatra::Base
   get '/test' do
     'Test page'
   end
+
+  get '/' do
+    'Welcome to Chitter'
+  end
+
+  get '/peeps' do
+    "This is a peep!"
+    erb: index
+  end
+
 
   run! if app_file == $0
 end
