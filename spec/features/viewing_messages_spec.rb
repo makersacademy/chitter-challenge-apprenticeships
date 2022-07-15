9b5d95cfe65fcfcbabc8308ca1c89adc630dbde7
olliefreeman94 <oliver.freeman@yahoo.com>
2022-07-15 10:51:34 +0100

    Add index page

---
+++ spec/features/viewing_messages_spec.rb
@@ -0,0 +1,14 @@
+feature 'viewing messages' do
+  scenario 'visiting the index page' do
+    visit('/')
+    expect(page).to have_content "Chitter"
+  end
+  
+  xscenario 'a user can see messages' do
+    visit('/peeps')
+
+    expect(page).to have_content "This is a peep!"
+    expect(page).to have_content "This is also a peep!"
+    expect(page).to have_content "This is a third peep!"
+  end
+end
